355d0813c7b0bba2a68b069fe02705dddfaeaa88
FatjonMileti <tonimileti@yahoo.com>
2023-05-02 08:45:51 +0200

    Initial commit

---
+++ server/db/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const url = process.env.URL;
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((e) => console.log("Error", e));
